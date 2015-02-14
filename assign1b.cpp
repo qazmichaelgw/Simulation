f16e7803c7b5c8f6c5a0de89a50096c075d9851a
GaoWei <qazmichaelgw@gmail.com>
2015-02-14 08:49:13 +0800

    test git

---
+++ assign1b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <algorithm>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+// define X->Y is forward
+const float k_mean[] = {12.0/60, 15.0/60};
+const float k_conversition_mean = 5;
+using namespace std;
+enum DIRECTION {FORWARD, BACKWARD, BOTH};
+class Channel
+{
+private:
+  DIRECTION direction_;
+
+};
+
+int main()
+{
+  return 0;
+}
